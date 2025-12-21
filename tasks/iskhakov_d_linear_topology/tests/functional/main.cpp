68a518a6ec654c14e47d398a964842084ed075eb
Ch0k0ladka <ch0ko0ladka@gmail.com>
2025-12-21 22:23:36 +0300

    fix: func + clang-format

--- tasks/iskhakov_d_linear_topology/tests/functional/main.cpp
+++ tasks/iskhakov_d_linear_topology/tests/functional/main.cpp
@@ -212,7 +212,7 @@ Message CreateMessage(int head, int tail, int data_size, bool delivered) {
   if (data_size > 0) {
     msg.data.resize(data_size);
     for (int vector_filling_step = 0; vector_filling_step < data_size; ++vector_filling_step) {
-      msg.data[vector_filling_step] = vector_filling_step + 1;
+      msg.data.push_back(i + 1);
     }
   }
   return msg;
